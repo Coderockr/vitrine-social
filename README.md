fbd81bc606e4f2d46f972b1a248a282291710aa0
Elton Minetto <eminetto@gmail.com>
2017-08-26 16:01:07 -0300

    Migrations

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vitrine-social
+
+# Migrations
+
+## Criar uma migration
+
+    sql-migrate new -config=./devops/dbconfig.yml -env=production default-categories
---
+++ devops/migrations/20170826155748-default-categories.sql
@@ -0,0 +1,11 @@
+
+-- +migrate Up
+insert into categories values (1, 'Roupas', 'http://placehold.it/10x10');
+insert into categories values (2, 'Brinquedos', 'http://placehold.it/10x10');
+insert into categories values (3, 'Alimentos', 'http://placehold.it/10x10');
+insert into categories values (4, 'Serviços', 'http://placehold.it/10x10');
+insert into categories values (5, 'Trabalho Voluntário', 'http://placehold.it/10x10');
+insert into categories values (6, 'Materiais', 'http://placehold.it/10x10');
+insert into categories values (7, 'Equipamentos', 'http://placehold.it/10x10');
+insert into categories values (8, 'Saúde', 'http://placehold.it/10x10');
+-- +migrate Down
