7477a3479c38123d9d9e538e14a5778309467368
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-03-18 12:53:54 -0300

    server/server to cmd/server

--- server/db/postgres.go
+++ server/db/postgres.go
@@ -3,11 +3,14 @@ package db
 import (
 	"database/sql"
 	"log"
+	"os"
 	"time"
 
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
+
+	// needed for sqlx to know how to connect
 	_ "github.com/lib/pq"
 )
 
@@ -15,7 +18,8 @@ type mDB struct {
 	db *sql.DB
 }
 
-type DBConfig struct {
+// Config informs the data needed for connecting to the database
+type Config struct {
 	User     string
 	Passwd   string
 	DBName   string
@@ -25,14 +29,14 @@ type DBConfig struct {
 }
 
 //InitDb faz a conexão com o banco
-func InitDb(dbConfig DBConfig) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbConfig.User, dbConfig.Passwd, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
+func InitDb(config Config) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.User, config.Passwd, config.DBHost, config.DBPort, config.DBName)
 	db, err := sqlx.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
 	var dbErr error
-	for attempt := 1; attempt < dbConfig.Attempts; attempt++ {
+	for attempt := 1; attempt < config.Attempts; attempt++ {
 		dbErr = db.Ping()
 		if dbErr == nil {
 			break
@@ -41,4 +45,17 @@ func InitDb(dbConfig DBConfig) (*sqlx.DB, error) {
 		time.Sleep(time.Duration(attempt) * time.Second)
 	}
 	return db, dbErr
+}
+
+// GetFromEnv will return the database connection based on the enviroment variables
+func GetFromEnv() (*sqlx.DB, error) {
+	dbConf := Config{
+		User:     os.Getenv("DATABASE_USER"),
+		Passwd:   os.Getenv("DATABASE_PASSWORD"),
+		DBName:   os.Getenv("DATABASE_NAME"),
+		DBHost:   os.Getenv("DATABASE_HOST"),
+		DBPort:   os.Getenv("DATABASE_PORT"),
+		Attempts: 10,
+	}
+	return InitDb(dbConf)
 }
