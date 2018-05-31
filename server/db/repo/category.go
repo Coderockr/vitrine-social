ae281dd9a91780e165f5819ed663104a966ed175
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-05-31 12:22:44 -0300

    needs count

--- server/db/repo/category.go
+++ server/db/repo/category.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"errors"
+
 	"github.com/golang/sync/syncmap"
 
 	"github.com/Coderockr/vitrine-social/server/model"
@@ -36,6 +38,22 @@ func (r *CategoryRepository) Get(id int64) (*model.Category, error) {
 
 	categoryCache.Store(id, c)
 	return c, nil
+}
+
+func (r *CategoryRepository) GetNeedsCount(c *model.Category) (int64, error) {
+	if c == nil {
+		return 0, errors.New("No Category informed !")
+	}
+
+	count := int64(0)
+
+	err := r.db.Get(
+		&count,
+		"SELECT COUNT(n.id) FROM needs n WHERE n.category_id = $1",
+		c.ID,
+	)
+
+	return count, err
 }
 
 // GetAll return all categories
