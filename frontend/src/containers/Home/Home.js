663e9cf8063825ce273fb59e1847f5313b98d8aa
Julia Possamai <juliabpp@hotmail.com>
2018-06-08 16:18:26 -0300

    Implemented get categories in the Home container

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -3,13 +3,45 @@ import Categories from '../../components/Categories';
 import Pagination from '../../components/Pagination';
 import Layout from '../../components/Layout';
 import Requests from '../../components/Requests';
+import api from '../../utils/api';
 
-const Home = () => (
-  <Layout>
-    <Categories />
-    <Requests />
-    <Pagination />
-  </Layout>
-);
+class Home extends React.Component {
+  state = {
+    loading: true,
+    categories: [],
+    requests: [],
+  }
+
+  componentWillMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories() {
+    api.get('categories').then(
+      (response) => {
+        this.setState({
+          categories: response.data,
+          loading: false,
+        });
+      },
+    );
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Categories
+          loading={this.state.loading}
+          categories={this.state.loading ? null : this.state.categories}
+        />
+        <Requests
+          loading={this.state.loading}
+          activeRequests={this.state.loading ? null : this.state.requests}
+        />
+        <Pagination />
+      </Layout>
+    );
+  }
+}
 
 export default Home;
