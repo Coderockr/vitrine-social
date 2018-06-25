b47048d995b0aedfa82810534b279de8eca7b23b
Anderson Zorrer Dalmina <andersondalmina@gmail.com>
2018-06-25 13:45:31 -0300

    Config travis to deploy backend and frontend

---
+++ devops/deploy.sh
@@ -0,0 +1,8 @@
+# Stop server service
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop vitrine-social'
+
+# Upload new compiled file
+scp -i ./devops/vitrinesocial.pem server/vitrine-social $DEPLOY_USER@$DEPLOY_HOST:~/vitrine-social/
+
+# Start serve service
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start vitrine-social'
