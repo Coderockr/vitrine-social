b4ea299cdbc9820df0a061eb0efc3cb085eb89b3
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-06-27 13:06:30 -0300

    DateTime scalar RFC 3339

--- server/graphql/scalar.go
+++ server/graphql/scalar.go
@@ -1,7 +1,6 @@
 package graphql
 
 import (
-	"log"
 	"time"
 
 	"github.com/graphql-go/graphql"
@@ -24,7 +23,6 @@ func serializeDate(value interface{}) interface{} {
 func unserializeDate(value interface{}) interface{} {
 	switch value := value.(type) {
 	case string:
-		log.Printf("string: %s", value)
 		t, err := time.Parse(dateFormat, value)
 		if err != nil {
 			return nil
@@ -50,6 +48,50 @@ var Date = graphql.NewScalar(graphql.ScalarConfig{
 		switch valueAST := valueAST.(type) {
 		case *ast.StringValue:
 			return unserializeDate(valueAST.Value)
+		}
+		return nil
+	},
+})
+
+func serializeDateTime(value interface{}) interface{} {
+	switch value := value.(type) {
+	case time.Time:
+		return value.Format(time.RFC3339)
+	case *time.Time:
+		return serializeDate(*value)
+	default:
+		return nil
+	}
+}
+
+func unserializeDateTime(value interface{}) interface{} {
+	switch value := value.(type) {
+	case string:
+		t, err := time.Parse(time.RFC3339, value)
+		if err != nil {
+			return nil
+		}
+
+		return t
+	case []byte:
+		return unserializeDate(string(value))
+	case *string:
+		return unserializeDate(string(*value))
+	default:
+		return nil
+	}
+}
+
+// DateTime is a scalar represents a Date time string supported by the RFC 3339.
+var DateTime = graphql.NewScalar(graphql.ScalarConfig{
+	Name:        "DateTime",
+	Description: "The `DateTime` scalar represents a Date time string supported by the RFC 3339.",
+	Serialize:   serializeDateTime,
+	ParseValue:  unserializeDateTime,
+	ParseLiteral: func(valueAST ast.Value) interface{} {
+		switch valueAST := valueAST.(type) {
+		case *ast.StringValue:
+			return unserializeDateTime(valueAST.Value)
 		}
 		return nil
 	},
