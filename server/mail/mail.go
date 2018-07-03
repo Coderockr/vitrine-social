976d4c0b43428157d2c020adc3e92d121a1d14ea
Anderson Zorrer Dalmina <andersondalmina@gmail.com>
2018-07-03 12:03:35 -0300

    Implement email send

---
+++ server/mail/mail.go
@@ -0,0 +1,43 @@
+package mail
+
+import (
+	"os"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+)
+
+// Mailer is a implementation to send emails
+type Mailer struct {
+	Dial gomail.SendCloser
+}
+
+// EmailParams struct with emails infos
+type EmailParams struct {
+	From    string
+	To      string
+	Subject string
+	Body    string
+}
+
+// Connect - Create and return a dialer
+func Connect() (Mailer, error) {
+	mailPort, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	d := gomail.NewDialer(os.Getenv("MAIL_HOST"), mailPort, os.Getenv("MAIL_USER"), os.Getenv("MAIL_PASS"))
+	s, err := d.Dial()
+
+	return Mailer{
+		Dial: s,
+	}, err
+}
+
+// SendEmail - Send email
+func (d Mailer) SendEmail(params EmailParams) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", params.From)
+	m.SetHeader("To", params.To)
+	m.SetHeader("Subject", params.Subject)
+	m.SetBody("text/html", params.Body)
+
+	return gomail.Send(d.Dial, m)
+}
