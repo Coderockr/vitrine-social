d79846bb2fee14924b3ba653a174b2af49d770ce
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-07-05 22:39:43 -0300

    use mock

--- server/storage/imageStorage_test.go
+++ server/storage/imageStorage_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/Coderockr/vitrine-social/server/storage"
 	testutil "github.com/Coderockr/vitrine-social/server/testutils"
 	"github.com/graymeta/stow"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -37,89 +38,98 @@ func TestCreateNeedImageShouldFail(t *testing.T) {
 	)
 
 	r.ParseMultipartForm(32 << 20)
-	nRAlwaysGet := &needRepositoryMock{
-		GetFN: func(id int64) (*model.Need, error) {
-			n := &model.Need{
-				ID:             id,
-				OrganizationID: 888,
-			}
 
-			return n, nil
-		},
-	}
+	genNeedRepoMock := func(id int64) *needRepositoryMock {
+		repo := &needRepositoryMock{}
+		n := &model.Need{
+			OrganizationID: 888,
+			ID:             id,
+		}
+		repo.On("Get", id).
+			Once().
+			Return(n, nil)
 
-	c := &containerMock{
-		PutFN: func(c *containerMock, name string, r io.Reader, size int64, metadata map[string]interface{}) (stow.Item, error) {
-			return nil, errors.New("fail to save it")
-		},
+		return repo
 	}
 
 	tests := map[string]test{
 		"when_need_not_exists": test{
-			token:     &model.Token{},
-			needID:    404,
-			fh:        nil,
-			err:       "there is no need with the id 404",
-			container: c,
-			needRepo: &needRepositoryMock{
-				GetFN: func(id int64) (*model.Need, error) {
-					return nil, errors.New("not found")
-				},
-			},
+			token:  &model.Token{},
+			needID: 404,
+			fh:     nil,
+			err:    "there is no need with the id 404",
+			needRepo: func() *needRepositoryMock {
+				repo := &needRepositoryMock{}
+				repo.On("Get", mock.AnythingOfType("int64")).
+					Return(nil, errors.New("not found"))
+				return repo
+			}(),
 		},
 		"when_org_does_not_own_need": test{
-			token:     &model.Token{UserID: 403},
-			needID:    405,
-			fh:        nil,
-			err:       "need 405 does not belong to organization 403",
-			container: c,
-			needRepo:  nRAlwaysGet,
+			token:    &model.Token{UserID: 403},
+			needID:   405,
+			fh:       nil,
+			err:      "need 405 does not belong to organization 403",
+			needRepo: genNeedRepoMock(405),
 		},
 		"when_fails_to_process_file": test{
-			token:     &model.Token{UserID: 888},
-			needID:    405,
-			fh:        &multipart.FileHeader{Filename: "upload.png"},
-			err:       "there was a problem with the file upload.png",
-			container: c,
-			needRepo:  nRAlwaysGet,
+			token:    &model.Token{UserID: 888},
+			needID:   405,
+			fh:       &multipart.FileHeader{Filename: "upload.png"},
+			err:      "there was a problem with the file upload.png",
+			needRepo: genNeedRepoMock(405),
 		},
 		"when_fails_to_load_to_container": test{
-			token:     &model.Token{UserID: 888},
-			needID:    405,
-			fh:        r.MultipartForm.File["to_fail"][0],
-			err:       "there was a problem saving the file imageStorage_test.go",
-			container: c,
-			needRepo:  nRAlwaysGet,
+			token:  &model.Token{UserID: 888},
+			needID: 405,
+			fh:     r.MultipartForm.File["to_fail"][0],
+			err:    "there was a problem saving the file imageStorage_test.go",
+			container: func() *containerMock {
+				c := &containerMock{}
+				c.On("Put", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Return(nil, errors.New("fail to save it"))
+				return c
+			}(),
+			needRepo: genNeedRepoMock(405),
 		},
 		"when_fails_to_save_to_database": test{
 			token:  &model.Token{UserID: 888},
 			needID: 405,
 			fh:     r.MultipartForm.File["not_to_fail"][0],
 			err:    "it have failed to save",
-			container: &containerMock{
-				pedingActions: "should have removed the item",
-				PutFN: func(c *containerMock, name string, r io.Reader, size int64, metadata map[string]interface{}) (stow.Item, error) {
-					i := itemMock{url: name, md: metadata}
-					return i, nil
-				},
-				RemoveItemFN: func(c *containerMock, id string) error {
-					c.pedingActions = ""
-					return nil
-				},
-			},
-			needRepo: &needRepositoryMock{
-				GetFN: func(id int64) (*model.Need, error) {
-					n := &model.Need{
-						ID:             id,
-						OrganizationID: 888,
-					}
+			container: func() *containerMock {
+				c := &containerMock{}
 
-					return n, nil
-				},
-				CreateImageFN: func(i model.NeedImage) (model.NeedImage, error) {
-					return i, errors.New("it have failed to save")
-				},
-			},
+				i := &itemMock{}
+				c.On("Put", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Run(func(args mock.Arguments) {
+						i.url = args.String(0)
+						i.md = args.Get(3).(map[string]interface{})
+
+						c.On("RemoveItem", i.url).Once().Return(nil)
+					}).
+					Return(i, nil)
+
+				return c
+			}(),
+			needRepo: func() *needRepositoryMock {
+				repo := &needRepositoryMock{}
+
+				repo.On("Get", int64(405)).
+					Return(
+						&model.Need{
+							ID:             405,
+							OrganizationID: 888,
+						},
+						nil,
+					)
+
+				call := repo.On("CreateImage", mock.Anything)
+				call.Run(func(args mock.Arguments) {
+					call.Return(args.Get(0).(model.NeedImage), errors.New("it have failed to save"))
+				})
+				return repo
+			}(),
 		},
 	}
 
@@ -133,83 +143,96 @@ func TestCreateNeedImageShouldFail(t *testing.T) {
 			_, err := iS.CreateNeedImage(p.token, p.needID, p.fh)
 			require.Equal(t, err.Error(), p.err)
 
-			if len(p.container.pedingActions) != 0 {
-				t.Error(p.container.pedingActions)
+			if p.container != nil {
+				p.container.AssertExpectations(t)
 			}
+			p.needRepo.AssertExpectations(t)
 		})
 
 	}
 }
 
 type needRepositoryMock struct {
-	GetFN            func(int64) (*model.Need, error)
-	CreateImageFN    func(model.NeedImage) (model.NeedImage, error)
-	GetNeedsImagesFN func(model.Need) ([]model.NeedImage, error)
-	DeleteImageFN    func(imageID, needID int64) error
+	mock.Mock
 }
 
-func (m *needRepositoryMock) Get(id int64) (*model.Need, error) {
-	return m.GetFN(id)
+func (n *needRepositoryMock) Get(id int64) (*model.Need, error) {
+	args := n.Called(id)
+	if need := args.Get(0); need != nil {
+		return need.(*model.Need), args.Error(1)
+	}
+	return nil, args.Error(1)
 }
 
-func (m *needRepositoryMock) CreateImage(ni model.NeedImage) (model.NeedImage, error) {
-	return m.CreateImageFN(ni)
+func (n *needRepositoryMock) CreateImage(ni model.NeedImage) (model.NeedImage, error) {
+	args := n.Called(ni)
+	return args.Get(0).(model.NeedImage), args.Error(1)
 }
 
-func (m *needRepositoryMock) GetNeedsImages(n model.Need) ([]model.NeedImage, error) {
-	return m.GetNeedsImages(n)
+func (n *needRepositoryMock) GetNeedsImages(need model.Need) ([]model.NeedImage, error) {
+	args := n.Called(need)
+	return args.Get(0).([]model.NeedImage), args.Error(1)
 }
 
-func (m *needRepositoryMock) DeleteImage(imageID, needID int64) error {
-	return m.DeleteImageFN(imageID, needID)
+func (n *needRepositoryMock) DeleteImage(imageID, needID int64) error {
+	args := n.Called(imageID, needID)
+	return args.Error(0)
 }
 
 type orgRepositoryMock struct {
-	GetFN         func(int64) (*model.Organization, error)
-	CreateImageFN func(model.OrganizationImage) (model.OrganizationImage, error)
-	DeleteImageFN func(imageID, orgID int64) error
+	mock.Mock
 }
 
 func (m *orgRepositoryMock) Get(id int64) (*model.Organization, error) {
-	return m.GetFN(id)
+	args := m.Called(id)
+	return args.Get(0).(*model.Organization), args.Error(1)
 }
 
 func (m *orgRepositoryMock) CreateImage(ni model.OrganizationImage) (model.OrganizationImage, error) {
-	return m.CreateImageFN(ni)
+	args := m.Called(ni)
+	return args.Get(0).(model.OrganizationImage), args.Error(1)
 }
 
 func (m *orgRepositoryMock) DeleteImage(imageID, orgID int64) error {
-	return m.DeleteImageFN(imageID, orgID)
+	args := m.Called(imageID, orgID)
+	return args.Error(0)
 }
 
 type containerMock struct {
-	pedingActions string
-	RemoveItemFN  func(c *containerMock, id string) error
-	PutFN         func(c *containerMock, name string, r io.Reader, size int64, metadata map[string]interface{}) (stow.Item, error)
+	mock.Mock
 }
 
 func (c *containerMock) ID() string {
-	return "containerMock"
+	args := c.Called()
+	return args.String(0)
 }
 
 func (c *containerMock) Name() string {
-	return c.ID()
+	args := c.Called()
+	return args.String(0)
 }
 
 func (c *containerMock) Item(id string) (stow.Item, error) {
-	return nil, nil
+	args := c.Called(id)
+	return args.Get(0).(stow.Item), args.Error(1)
 }
 
 func (c *containerMock) Items(prefix, cursor string, count int) ([]stow.Item, string, error) {
-	return make([]stow.Item, 0), "", nil
+	args := c.Called(prefix, cursor, count)
+	return args.Get(0).([]stow.Item), args.String(1), args.Error(3)
 }
 
 func (c *containerMock) RemoveItem(id string) error {
-	return c.RemoveItemFN(c, id)
+	args := c.Called(id)
+	return args.Error(0)
 }
 
 func (c *containerMock) Put(name string, r io.Reader, size int64, metadata map[string]interface{}) (stow.Item, error) {
-	return c.PutFN(c, name, r, size, metadata)
+	args := c.Called(name, r, size, metadata)
+	if i := args.Get(0); i == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(stow.Item), args.Error(1)
 }
 
 type itemMock struct {
