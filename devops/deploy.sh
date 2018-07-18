8e0a2e89788b64c324975f64c7d75395a62aa604
Anderson Zorrer Dalmina <andersondalmina@gmail.com>
2018-07-18 11:04:40 -0300

    Caddyfile config

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 # Stop server service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop vitrine-social'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop caddy vitrine-social'
+
+# Upload new Caddy config file
+scp -i ./devops/vitrinesocial.pem devops/Caddyfile $DEPLOY_USER@$DEPLOY_HOST:~/
 
 # Upload new compiled file
 scp -i ./devops/vitrinesocial.pem server/vitrine-social $DEPLOY_USER@$DEPLOY_HOST:~/vitrine-social/
 
 # Start serve service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start vitrine-social'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start caddy vitrine-social'
 
 # Run Migrations
 sql-migrate up -config=devops/dbconfig.yml -env=production
