5694682d8872aaca501cb5d3687ccc99b8dad9f0
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2018-07-26 11:48:44 -0300

    :hankey:

--- devops/migrations/20180719205136-organization-logo.sql
+++ devops/migrations/20180719205136-organization-logo.sql
@@ -1,7 +1,10 @@
 
 -- +migrate Up
-ALTER TABLE organizations ALTER COLUMN logo TYPE INTEGER USING (logo::integer);
+ALTER TABLE organizations ALTER COLUMN logo DROP NOT NULL;
+ALTER TABLE organizations ALTER COLUMN logo SET DEFAULT 0;
+UPDATE organizations SET logo = NULL;
 ALTER TABLE organizations RENAME logo TO logo_image_id;
+ALTER TABLE organizations ALTER COLUMN logo_image_id SET DATA TYPE INTEGER USING logo_image_id::integer;
 ALTER TABLE organizations ADD FOREIGN KEY(logo_image_id) REFERENCES organizations_images(id) ON UPDATE CASCADE ON DELETE RESTRICT;
 ALTER TABLE organizations ALTER COLUMN logo_image_id DROP NOT NULL;
 ALTER TABLE organizations ALTER COLUMN logo_image_id SET DEFAULT NULL;
