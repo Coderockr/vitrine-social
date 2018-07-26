25669fc315ea8a0bf777697f1244e71912942bc8
Anderson Zorrer Dalmina <andersondalmina@gmail.com>
2018-07-26 09:22:09 -0300

    Migration not change the slug

--- devops/migrations/20180725183608-update-categories.sql
+++ devops/migrations/20180725183608-update-categories.sql
@@ -1,10 +1,10 @@
 
 -- +migrate Up
-UPDATE categories SET name='Higiene Pessoal', slug='higiene-pessoal' WHERE name='Higiene';
+UPDATE categories SET name='Higiene Pessoal' WHERE name='Higiene';
 INSERT INTO categories (name, slug) VALUES ('Material de Limpeza', 'limpeza');
 ALTER TABLE organizations ADD website VARCHAR(255) DEFAULT NULL;
 
 -- +migrate Down
-UPDATE categories SET name='Higiene', slug='higiene' WHERE name='Higiene Pessoal';
+UPDATE categories SET name='Higiene' WHERE name='Higiene Pessoal';
 DELETE FROM categories WHERE slug = 'limpeza';
 ALTER TABLE organizations DROP COLUMN website;
