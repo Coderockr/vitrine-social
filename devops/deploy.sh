2998b3f88b42bfcfc67636f69469b8ebab98c747
Anderson Zorrer Dalmina <andersondalmina@gmail.com>
2018-08-28 15:48:20 -0300

    Docker compose file and changed deploy script

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # Stop server service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop caddy vitrine-social'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop caddy vitrine-social && docker-compose down'
 
-# Upload new Caddy config file
+# Upload new Caddy config file and docker-compose
 scp -i ./devops/vitrinesocial.pem devops/Caddyfile $DEPLOY_USER@$DEPLOY_HOST:~/
+scp -i ./devops/vitrinesocial.pem devops/docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/
 
 # Upload new compiled file
 scp -i ./devops/vitrinesocial.pem server/vitrine-social $DEPLOY_USER@$DEPLOY_HOST:~/vitrine-social/
 
 # Start serve service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start caddy vitrine-social'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start caddy vitrine-social && docker-compose up -d'
 
 # Run Migrations
 sql-migrate up -config=devops/dbconfig.yml -env=production
