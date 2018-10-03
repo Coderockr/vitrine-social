22b48a010520826bad5c2d31c74a20d9d0f10b01
Felipe Caputo <felipe.caputo@gmail.com>
2018-10-03 00:10:07 -0300

    Versão 2 do handler de 404

--- frontend/src/containers/NotFound/NotFound.js
+++ frontend/src/containers/NotFound/NotFound.js
@@ -17,9 +17,13 @@ const NotFound = () => (
         <div className={styles.sectionWrapper}>
           <h1 className={styles.title}>404</h1><br />
           <h2>
-            A página que voce esta procurando não pode ser encontrada.<br />
-            <Link to="/" className={styles.link}>Clique aqui</Link> para retornar para a página inicial.
+            O link que você clicou parece estar quebrado ou a página pode ter sido removida.
           </h2>
+          <h3>
+            Visite nossa <Link to="/" className={styles.link}>Página Inicial</Link> ou
+            &nbsp;<Link to="/contato" className={styles.link}>entre em contato</Link> conosco
+            para relatar um problema.
+          </h3>
         </div>
       </Col>
     </Row>
