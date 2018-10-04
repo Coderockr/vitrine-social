6259cd3b81318d3dca7d68958bd3b295c8c1b66a
Leonardo Nascimento <leo386386@gmail.com>
2018-10-04 13:42:21 -0300

    Refactoring Arrow component

--- frontend/src/components/Arrow/Arrow.js
+++ frontend/src/components/Arrow/Arrow.js
@@ -3,62 +3,36 @@ import cx from 'classnames';
 import Icon from '../../components/Icons';
 import styles from './styles.module.scss';
 
-class Arrow extends React.PureComponent {
-  renderIcon() {
-    if (this.props.left) {
-      return (
-        <div
-          className={cx(styles.arrowButton, { [styles.arrowButtonOver]: this.props.over })}
-        >
-          <Icon
-            icon="arrow-left-drop-circle-outline"
-            size={this.props.size}
-            color={this.props.color}
-            className={cx(styles.imgBottom, { [styles.imgOver]: this.props.over })}
-          />
-          <Icon
-            icon="arrow-left-drop-circle"
-            size={this.props.size}
-            color={this.props.color}
-            className={cx(styles.imgTop, { [styles.imgOver]: this.props.over })}
-          />
-        </div>
-      );
-    }
-    return (
-      <div
-        className={cx(styles.arrowButton, { [styles.arrowButtonOver]: this.props.over })}
-      >
-        <Icon
-          icon="arrow-right-drop-circle-outline"
-          size={this.props.size}
-          color={this.props.color}
-          className={cx(styles.imgBottom, { [styles.imgOver]: this.props.over })}
-        />
-        <Icon
-          icon="arrow-right-drop-circle"
-          size={this.props.size}
-          color={this.props.color}
-          className={cx(styles.imgTop, { [styles.imgOver]: this.props.over })}
-        />
-      </div>
-    );
-  }
+const getIcon = left => (left ? 'left' : 'right');
 
-  render() {
-    return (
-      <div
-        role="button"
-        tabIndex={0}
-        onClick={this.props.onClick}
-        onKeyPress={this.props.onClick}
-        className={styles.arrowButton}
-        hidden={this.props.hidden}
-      >
-        {this.renderIcon()}
-      </div>
-    );
-  }
-}
+const Arrow = ({
+  size, color, onClick, hidden, over, left,
+}) => (
+  <div
+    role="button"
+    tabIndex={0}
+    onClick={onClick}
+    onKeyPress={onClick}
+    className={styles.arrowButton}
+    hidden={hidden}
+  >
+    <div
+      className={cx(styles.arrowButton, { [styles.arrowButtonOver]: over })}
+    >
+      <Icon
+        icon={`arrow-${getIcon(left)}-drop-circle-outline`}
+        size={size}
+        color={color}
+        className={cx(styles.imgBottom, { [styles.imgOver]: over })}
+      />
+      <Icon
+        icon={`arrow-${getIcon(left)}-drop-circle`}
+        size={size}
+        color={color}
+        className={cx(styles.imgTop, { [styles.imgOver]: over })}
+      />
+    </div>
+  </div>
+);
 
 export default Arrow;
