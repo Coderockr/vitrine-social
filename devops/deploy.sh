5da18440d77c7e6ab4114557fe789645df911f4f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-10-22 16:06:35 -0300

    desativar metabase

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # Stop server service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop caddy vitrine-social && docker-compose down'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl stop caddy vitrine-social'
 
 # Upload new Caddy config file and docker-compose
 scp -i ./devops/vitrinesocial.pem devops/Caddyfile $DEPLOY_USER@$DEPLOY_HOST:~/
-scp -i ./devops/vitrinesocial.pem devops/docker-compose.yml $DEPLOY_USER@$DEPLOY_HOST:~/
 
 # Upload new compiled file
 scp -i ./devops/vitrinesocial.pem server/vitrine-social $DEPLOY_USER@$DEPLOY_HOST:~/vitrine-social/
 
 # Start serve service
-ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start caddy vitrine-social && docker-compose up -d'
+ssh -i ./devops/vitrinesocial.pem -t $DEPLOY_USER@$DEPLOY_HOST 'sudo systemctl start caddy vitrine-social'
 
 # Run Migrations
 sql-migrate up -config=devops/dbconfig.yml -env=production
