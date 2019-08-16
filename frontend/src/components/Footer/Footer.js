6c1d7aa475ad36886e915c495b2ccc07085f17b3
Raony Marcondes <raonymarcondes@gmail.com>
2019-08-15 23:15:08 -0300

    Update the footer date automatically

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -3,26 +3,35 @@ import { Row, Col, Layout, Icon } from 'antd';
 import cx from 'classnames';
 import styles from './styles.module.scss';
 
-const Footer = ({ className }) => (
-  <Layout.Footer className={cx(styles.appFooter, className)}>
-    <Row>
-      <Col
-        xxl={{ span: 14, offset: 5 }}
-        xl={{ span: 20, offset: 2 }}
-        lg={{ span: 22, offset: 1 }}
-        md={{ span: 24, offset: 0 }}
-      >
-        <div className={styles.footerWrapper}>
-          <p className={styles.copyright}>Vitrine Social ©2018 Created by  <a target="_blank" rel="me" href="http://www.coderockr.com">Coderockr</a></p>
-          <div>
-            <a target="_blank" rel="me" href="https://www.facebook.com/avitrinesocial/"><Icon type="facebook" className={styles.icon} /></a>
-            <a target="_blank" rel="me" href="https://www.instagram.com/avitrine.social/"><Icon type="instagram" className={styles.icon} /></a>
-            <a target="_blank" rel="me" href="https://twitter.com/@avitrinesocial"><Icon type="twitter" className={styles.icon} /></a>
+const Footer = ({ className }) => {
+  const year = new Date();
+
+  return (
+    <Layout.Footer className={cx(styles.appFooter, className)}>
+      <Row>
+        <Col
+          xxl={{ span: 14, offset: 5 }}
+          xl={{ span: 20, offset: 2 }}
+          lg={{ span: 22, offset: 1 }}
+          md={{ span: 24, offset: 0 }}
+        >
+          <div className={styles.footerWrapper}>
+            <p className={styles.copyright}>
+              Vitrine Social ©{year.getFullYear()} Created by
+              <a target="_blank" rel="me" href="http://www.coderockr.com">
+                Coderockr
+              </a>
+            </p>
+            <div>
+              <a target="_blank" rel="me" href="https://www.facebook.com/avitrinesocial/"><Icon type="facebook" className={styles.icon} /></a>
+              <a target="_blank" rel="me" href="https://www.instagram.com/avitrine.social/"><Icon type="instagram" className={styles.icon} /></a>
+              <a target="_blank" rel="me" href="https://twitter.com/@avitrinesocial"><Icon type="twitter" className={styles.icon} /></a>
+            </div>
           </div>
-        </div>
-      </Col>
-    </Row>
-  </Layout.Footer>
-);
+        </Col>
+      </Row>
+    </Layout.Footer>
+  );
+};
 
 export default Footer;
